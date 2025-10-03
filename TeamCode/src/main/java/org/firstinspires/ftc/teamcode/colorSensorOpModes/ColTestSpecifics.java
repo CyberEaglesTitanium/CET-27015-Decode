b5ae2e32919dd6314ec5fe64d21950a045f2be27
ZGStuff <therealzapgames989@gmail.com>
2025-10-02 19:00:31 -0600

    Pride in Colors (as in color sensor)
    MORE
    COLOR
    SENSOR
    CODE!
    Sweeper code is being prepared for usage (not ready yet tho :C)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorOpModes/ColTestSpecifics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorOpModes/ColTestSpecifics.java
@@ -1,11 +1,9 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.colorSensorOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
 
 @TeleOp
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorOpModes/ColorSensorSorterThing.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.colorSensorOpModes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+@TeleOp
+public class ColorSensorSorterThing extends LinearOpMode {
+    private NormalizedColorSensor colsense;
+
+    @Override
+    public void runOpMode() {
+        colsense = hardwareMap.get(NormalizedColorSensor.class, "colsense");
+
+        waitForStart();
+
+        NormalizedRGBA colors = colsense.getNormalizedColors();
+
+        while (colors.blue >= 100 && colors.red <= 192) {
+            telemetry.addLine("Color is PURPLE");
+        }
+        while (colors.green == 255) {
+            telemetry.addLine("Color is GREEN");
+        }
+    }
+}
