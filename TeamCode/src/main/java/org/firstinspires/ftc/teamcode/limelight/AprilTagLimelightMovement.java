80e605b5a2d8f605db46eb214fd5bcbb20e357d5
ZGStuff <therealzapgames989@gmail.com>
2025-10-30 20:15:05 -0600

    "You cast: Compact!"
    Compacted and cleaned up some code.
    shhhhh the spaghetti code doesnt exist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight/AprilTagLimelightMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight/AprilTagLimelightMovement.java
@@ -18,7 +18,11 @@ import java.util.List;
 public class AprilTagLimelightMovement extends OpMode {
     private Limelight3A limelight;
     private IMU imu;
+    private LLResult llResult;
+    private Pose3D botPose;
 
+    private double botX;
+    private double botY;
 
     @Override
     public void init() {
@@ -41,28 +45,83 @@ public class AprilTagLimelightMovement extends OpMode {
 
     @Override
     public void loop() {
-        // Variables for YPR (yaw, pitch, roll), Limelight orientation, and Limelight results
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        limelight.updateRobotOrientation(orientation.getYaw());
-        LLResult llResult = limelight.getLatestResult();
+
+        // Limelight initialization
+        llResult = limelightInit();
 
         // Limelight loop, detects AprilTags and robot position
-        if (llResult != null & llResult.isValid()) {
-            Pose3D botPose = llResult.getBotpose_MT2();
+        if (validAprilTag()) {
+            botPose = llResult.getBotpose_MT2();
+
+            // Bot detection loop
             if (botPose != null) {
-                double x = botPose.getPosition().x;
-                double y = botPose.getPosition().y;
-                telemetry.addData("MT2 Location:", "(" + x + ", " + y + ")");
+                getBotPos();
             }
 
-            List<LLResultTypes.FiducialResult> fiducialResults = llResult.getFiducialResults();
-            for (LLResultTypes.FiducialResult fr : fiducialResults) {
-                if (fr.getFiducialId() == 20) {
-                    
-                }
-                telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
-            }
+            // Get necessary robot telemetry
+            botTelemetry();
             telemetry.update();
+        }
+    }
+
+    /*
+        Purpose: Initializes Limelight for use later and grabs latest results
+        Arguments: none
+        Returns: Latest Limelight result (object with all valid data received by a limelight)
+     */
+
+    public LLResult limelightInit() {
+
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+
+        return limelight.getLatestResult();
+    }
+
+    /*
+        Purpose: Checks that the current AprilTag is valid using pipeline filter
+        Arguments: none
+        Returns: boolean (result is valid)
+        Global variables:
+            - llResult: Array in llResult to check for valid AprilTag list
+     */
+    public boolean validAprilTag()  {
+        // Check for valid Limelight result
+        return llResult != null & llResult.isValid();
+    }
+
+    /*
+        Purpose: Gets current bot position as a Vector2 (x,y) using MegaTag2
+        Arguments: still none
+        Returns: double
+        Global variables:
+            - botX: Current robot X position in relation to estimated location on field
+            - botY: Current robot Y position in relation to estimated location on field
+     */
+    public void getBotPos() {
+        // Get bot positions for later use
+        botX = botPose.getPosition().x;
+        botY = botPose.getPosition().y;
+    }
+
+    /*
+        Purpose: Grabs telemetry of robot position and AprilTag ID, family, and position
+        Arguments: still *beep*ing none
+        Returns: "technically" nothing
+        Global variables:
+            - botX: Current robot X position in relation to estimated location on field
+            - botY: Current robot Y position in relation to estimated location on field
+            - llResult: Current Limelight results (see limelightInit)
+            - telemetry: Global telemetry object, adds data to update on telemetry.update()
+     */
+    public void botTelemetry () {
+        // Get robot location using MegaTag 2 and bot positions
+        telemetry.addData("MT2 Location:", "(" + botX + ", " + botY + ")");
+
+        // Get AprilTag IDs, positions, and family
+        List<LLResultTypes.FiducialResult> fiducialResults = llResult.getFiducialResults();
+        for (LLResultTypes.FiducialResult fr : fiducialResults) {
+            telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
         }
     }
 }
