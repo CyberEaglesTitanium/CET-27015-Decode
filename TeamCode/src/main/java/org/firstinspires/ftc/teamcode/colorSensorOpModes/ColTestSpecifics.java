05a15d75541916fb010df25ef5591480d42226c1
ZGStuff <therealzapgames989@gmail.com>
2025-10-30 18:38:00 -0600

    more! color! testing! yeah!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorOpModes/ColTestSpecifics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorOpModes/ColTestSpecifics.java
@@ -4,25 +4,30 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class ColTestSpecifics extends LinearOpMode {
     private ColorSensor colsense;
+    private Servo sweepingSweeper;
 
     @Override
     public void runOpMode() {
         colsense = hardwareMap.get(ColorSensor.class, "colsense");
+        sweepingSweeper = hardwareMap.get(Servo.class, "sweepingSweeper");
         waitForStart();
 
         while (opModeIsActive()) {
-            while (colsense.blue() <= 113 && colsense.red() <= 90 && colsense.green() >= 76) {
+            while (colsense.green() < colsense.red()) {
                 telemetry.clear();
                 telemetry.addLine("PURPLE FOUND");
+                sweepingSweeper.setPosition(1);
                 telemetry.update();
             }
-            while (colsense.green() <= 394 && colsense.red() <= 128 && colsense.blue() <= 243) {
+            while (colsense.green() > colsense.red()) {
                 telemetry.clear();
                 telemetry.addLine("GREEN FOUND");
+                sweepingSweeper.setPosition(-1);
                 telemetry.update();
             }
         }
